a27e7f1b61b86c1a15497a2e92dba3c433a4930e
tomerkl22 <tomerkl22@gmail.com>
2022-12-31 18:15:01 +0200

    Final

--- logic.js
+++ logic.js
@@ -1,91 +1,92 @@
-const winningMessageElement = document.getElementById('winningMessageText')
-const restartButton = document.getElementById('restartButton')
-const currentTurn = document.getElementById("currTurn")
-const cellElements = Array.from(document.getElementsByClassName("cell"))
-const winningCombinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+let turnPlayerText = document.getElementById("Turn-text")
+let cells = Array.from(document.getElementsByClassName("cell"))
+let winningMessage = document.getElementById("winningMessageText")
+let restartButton = document.getElementById("restartButton")
 
-let board = Array(9).fill(null)
+const playerX = "X"
+const playerO = "O"
 
-const player_X = "X"
-const player_O = "O"
-let currPlayer = player_X
+let count = 0
+let currPlayer = playerX
+turnPlayerText.innerHTML = "Turn " + playerX
+let cellsArray = new Array(9).fill(null)
+let gameRemaining = true
 
-const cellClicked = (event) => {
-    const id = event.target.id
-    let winningCombination
+const startGame = () =>{
+    cells.forEach(cell => cell.addEventListener("click", cellClicked))
+    restartButton.addEventListener("click",  restartTheGame)
+}
 
-    if (!board[id - 1]) {
-        board[id - 1] = currPlayer
-        cellElements[id - 1].innerHTML = currPlayer
-        currPlayer = currPlayer === player_X ? player_O : player_X
-        var color = currPlayer === player_X ? "red" : "blue"
-        currentTurn.style.setProperty('color', color)
-        cellElements[id - 1].style.setProperty('color', color)
-        winningCombination = checkIfThereIsAWinner()
-        if(winningCombination){
-            removeEventListeners();
-            colorWinningsCombination(winningCombination)
-            winningMessageElement.innerHTML = "THE WINNER IS " + currPlayer  
+function cellClicked(e){
+    let id=e.target.id
+    if (cellsArray[id] == null){
+        cells[id].innerHTML = currPlayer
+        cellsArray[id] = currPlayer
+        if (currPlayer == playerX){
+            count = count + 1
+            currPlayer = playerO
+            turnPlayerText.innerHTML = "Turn " + playerO
         }
         else{
-            currentTurn.textContent = currPlayer + " TURN"
+            count = count + 1
+            currPlayer = playerX
+            turnPlayerText.innerHTML = "Turn " + playerX
         }
     }
-}
+    checkWhoWon(0,1,2)
+    checkWhoWon(3,4,5)
+    checkWhoWon(6,7,8)
+    checkWhoWon(0,3,6)
+    checkWhoWon(1,4,7)
+    checkWhoWon(2,5,8)
+    checkWhoWon(0,4,8)
+    checkWhoWon(2,4,6)
 
-function colorWinningsCombination (winningCombination) {
-        const color = getComputedStyle(document.documentElement).getPropertyValue('--winning-color')
-    console.log("color" + color)
-    console.log(winningCombination)
-    winningCombination.forEach((cell)=>{
-        document.getElementById(cell).style.setProperty('color', color)
-    })
-}
-function removeEventListeners(){
-    cellElements.forEach((cell) =>{
-        cell.removeEventListener('click', cellClicked)
-    })
-}
-function checkIfThereIsAWinner(){
-    let winningCombination = null
+    if(gameRemaining == false)
+        cells.forEach(cell => cell.removeEventListener("click", cellClicked)) 
     
-    winningCombinations.forEach(combination => {
-        if (board[combination[0] - 1] == currPlayer
-            && board[combination[1] - 1] == currPlayer
-             && board[combination[2] - 1] == currPlayer) {
-             winningCombination = combination
-        }
-    });
-
-    return winningCombination
+  
+    if (count == 9 && gameRemaining == true) // All cells are full without a winner - DRAW
+         winningMessage.innerHTML = "DRAW"
+        
 }
 
-const restartGame = () =>{
-    board.fill(null)
-    cellElements.forEach((cell)=>{
-        cell.textContent = ""
-    })
 
-    winningMessageElement.textContent = ""
-    colorOriginalColor()
+function restartTheGame(){
+    cells.forEach(cell => cell.innerHTML=null)
+    cellsArray.fill(null)
+    winningMessage.innerHTML = ""
+    for (let i=0; i<9; i++)
+        cells[i].style.backgroundColor = 'white'
+    gameRemaining = true
+    count = 0
     startGame()
 }
 
-function colorOriginalColor(){
-    const originalColor = getComputedStyle(document.documentElement).getPropertyValue("--originalColor")
-    cellElements.forEach((cell) =>{
-        cell.style.setProperty('color', originalColor)
-    })
+
+function checkWinner(cell){
+    if (cell == playerO){
+       winningMessage.style.fontWeight = 'bold'
+       winningMessage.innerHTML = "Player O WON"
+    }
+    else{
+       winningMessage.style.fontWeight = 'bold'
+       winningMessage.innerHTML = "Player W WON"
+    }
 }
 
-restartButton.addEventListener('click', restartGame)
+function drawWinnerCells(firstCell, secondCell, thirdCell){
+    cells[firstCell].style.backgroundColor = 'green'
+    cells[secondCell].style.backgroundColor = 'green'
+    cells[thirdCell].style.backgroundColor = 'green'
+}
 
-const startGame = () => {
-    currentTurn.textContent = currPlayer + " TURN"
-
-    cellElements.forEach(element => {
-        element.addEventListener('click', cellClicked)
-    });
+function checkWhoWon(firstCell, secondCell, thirdCell){
+    if (cellsArray[firstCell] ==  cellsArray[secondCell] && cellsArray[secondCell] == cellsArray[thirdCell] && cellsArray[thirdCell] != null){
+        checkWinner(cellsArray[firstCell])
+        drawWinnerCells(firstCell,secondCell,thirdCell)
+        gameRemaining = false
+}
 }
 
 startGame()
